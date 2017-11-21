3dfc939dd2dc3dad6bb410e96aa290bc1984402a
365556520 <33852383+365556520@users.noreply.github.com>
2017-11-21 23:22:44 +0800

    11.21 23:22

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Http\Model\Member;
-use App\Http\Model\Pdt_Images;
-use App\Http\Model\Pdt_Content;
-use App\Http\Model\Product;
+
 use App\Http\Model\Category;
 Route::get('/', function () {
     return Category::all();
