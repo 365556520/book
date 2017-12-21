b76928f613d9cc84378e391d2f3b4ab8256c0cfb
365556520 <33852383+365556520@users.noreply.github.com>
2017-12-21 21:38:54 +0800

    2

--- app/Http/Controllers/View/MemBerController.php
+++ app/Http/Controllers/View/MemBerController.php
@@ -10,6 +10,7 @@ class MemBerController extends Controller
 {
     public function login(Request $request)
     {  $return_url = $request->input('return_url','');
+        //urldecode给地址解密
         return view('login')->with('return_url',urldecode($return_url));
     }
 
--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -11,10 +11,12 @@ class CheckLogin
     public function handle($request,Closure $next)
     {
         if(isset($_SERVER['HTTP_REFERER'])){
+            //接来上一个连接存在就赋值
             $http_referer = $_SERVER['HTTP_REFERER'];
         }
        $member = $request->session()->get('member','');
         if ($member == ''){
+            //urlencode给地址加密
             return redirect('login?return_url='.urlencode($http_referer));
         }
         return $next($request);
